5c5669fc547d1a097c843f2e9f2b9e4cde9d5279
andori8 <falcsmike7@gmail.com>
2019-01-04 11:00:28 -0500

    defined home component and added routing buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Search from './containers/Search';
 import RecentPhotos from './containers/RecentPhotos';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 import ShowPhoto from './containers/ShowPhoto';
 import './App.css';
 
@@ -13,6 +14,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <NavBar />
+          <Route exact path="/" component={Home} />
           <Route exact path="/search" component={Search} />
           <Route exact path="/recent" component={RecentPhotos} />
           <Route exact path="/recent/:id" component={ShowPhoto} />
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class Home extends Component {
+  state = {
+    redirect: false,
+    path: ""
+  }
+
+  handleClick = e => {
+    this.setState({
+      redirect: true,
+      path: e.target.id
+    })
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to={`/${this.state.path}`} />
+    }
+  }
+  render() {
+    return (
+      <div className="home">
+        {this.renderRedirect()}
+        <button id="search" onClick={this.handleClick}>Search</button>
+        <button id="recent" onClick={this.handleClick}>Most Recent</button>
+      </div>
+    )
+  }
+}
+
+export default Home;
