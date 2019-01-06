fff7788b507bd6647b64e22f1bbe762132bfe71c
andori8 <falcsmike7@gmail.com>
2019-01-06 10:41:46 -0500

    npm run build

--- src/actions/fetchPhotos.js
+++ src/actions/fetchPhotos.js
@@ -1,7 +1,7 @@
 export function fetchPhotos() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_PHOTOS' });
-    return fetch('http://localhost:3001/api/v1/recent')
+    return fetch('https://flickstr-react-redux.herokuapp.com/api/v1/recent')
     .then(res => res.json())
     .then(photos => dispatch( {type: 'FETCH_PHOTOS', payload: photos.photos.photo }))
   }
@@ -11,7 +11,7 @@ export function fetchSearchedPhotos(q) {
   return (dispatch) => {
     dispatch({ type: 'LOADING_SEARCHED_PHOTOS' });
     if (q.length !== 0) {
-    return fetch(`http://localhost:3001/api/v1/search`, {
+    return fetch(`https://flickstr-react-redux.herokuapp.com/api/v1/search`, {
       method: 'POST',
       headers: {"content-type": "application/json"},
       body: JSON.stringify({query: q})
@@ -25,7 +25,7 @@ export function fetchSearchedPhotos(q) {
 export function fetchClickedPhoto(id) {
   return (dispatch) => {
     dispatch({ type: 'LOADING_CLICKED_PHOTO' });
-    return fetch(`http://localhost:3001/api/v1/recent/${id}`)
+    return fetch(`https://flickstr-react-redux.herokuapp.com/api/v1/recent/${id}`)
       .then(res => res.json())
       .then(photo => dispatch({ type: 'FETCH_CLICKED_PHOTO', payload: photo }))
   }
