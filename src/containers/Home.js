df4a21fc2ff7e4217b55c3988674abb7c84e7ab3
andori8 <falcsmike7@gmail.com>
2019-01-24 13:25:23 -0500

    got rid of redundant code in home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,27 +5,9 @@ import { connect } from 'react-redux';
 import {fetchSearchedPhotos} from '../actions/fetchPhotos';
 
 class Home extends Component {
-  state = {
-    redirect: false,
-    path: ""
-  }
-
-  handleClick = e => {
-    this.setState({
-      redirect: true,
-      path: e.target.id
-    })
-  }
-
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Redirect to={`/${this.state.path}`} />
-    }
-  }
   render() {
     return (
       <div className="home" >
-        {this.renderRedirect()}
         <Welcome fetchPhotos={this.props.fetchSearchedPhotos}/>
       </div>
     )
